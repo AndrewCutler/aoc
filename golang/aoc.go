66efb364055323e8ada6b52f4e5ff89a5b2e15a7
Andrew Cutler <Andrew.Cutler@fusionmgt.com>
2023-07-26 22:50:27 -0400

    did day1 in go

--- golang/aoc.go
+++ golang/aoc.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("test")
+	day1()
+}
+
+func day1() {
+	file, err := os.Open("../data/day1.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	curr := 0
+	sums := make([]int, 0)
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			sums = append(sums, curr)
+			curr = 0
+			continue
+		}
+
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		curr += val
+	}
+	sort.Ints(sums)
+	len := len(sums)
+	partOne := sums[len-1]
+	second := sums[len-2]
+	third := sums[len-3]
+	partTwo := partOne + second + third
+
+	if partOne != 68775 {
+		panic("part one is incorrect")
+	}
+	if partTwo != 202585 {
+		panic("part two is incorrect")
+	}
+
+	fmt.Println("Huzzah!")
 }
