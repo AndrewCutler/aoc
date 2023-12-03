c945514ff4de72809e4d854f88dd0dffe4f1fdaa
Andrew Cutler <Andrew.Cutler@fusionmgt.com>
2023-12-03 14:42:49 -0500

    solved day1 part1

--- 2023/golang/days/day1.go
+++ 2023/golang/days/day1.go
@@ -1,17 +1,45 @@
 package days
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 func DayOne() {
 	file, err := os.Open("../data/day1.txt")
-
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 	defer file.Close()
 
-	fmt.Println("Day one:")
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var first rune
+		var second rune
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				if !unicode.IsPrint(first) {
+					first = char
+				}
+				second = char
+			}
+		}
+
+		combined := string([]rune{first, second})
+		converted, err := strconv.Atoi(combined)
+		if err != nil {
+			panic(err)
+		}
+
+		total += converted
+	}
+
+	fmt.Printf("Day one: %d\n", total)
 }
